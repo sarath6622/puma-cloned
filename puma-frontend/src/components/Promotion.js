982ed47d683d070c49b6753cbcc0d7fa24a503f1
sarath6622 <sarathsanthan6622@gmail.com>
2023-09-10 11:31:20 +0530

    updated src with components folder

---
+++ puma-frontend/src/components/Promotion.js
@@ -0,0 +1,12 @@
+import './Promotion.css';
+
+function Promotion() {
+  return (
+    <div className="Promotion">
+      <h3>Extra 10% off on orders above ₹3499</h3>
+      <p>Discount auto-applied at checkout</p>
+    </div>
+  );
+}
+
+export default Promotion;
